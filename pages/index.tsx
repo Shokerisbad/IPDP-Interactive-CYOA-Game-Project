9197faf27bfc3859343410c0fd854fcbda145b79
ShokerIsBad <mihalache.petre.cristi@gmail.com>
2025-03-10 16:06:39 +0200

    ITP-18 : Create Story Node Logic based on the Factory Design Pattern Changed the .gitignore to take the .next folder too, it's generated automatically. ------------ I've created the logic using an API that fetches the story nodes from a json, i've not added the factory design pattern, that's gonna be in a development tool for adding story nodes. ------------ I've also added to the react page, added global css and a styles-css-modules for modifying css based on classes Changed the project structure a little bit by putting everything where it needs to be, data in the data folder, pages in the page folder etc. ------------ Also added a landing page that, for now, only takes you to the game page

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import styles from './styles.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.startContainer} style={{ textAlign: 'center', padding: '50px' }}>
+            <h1>Welcome to the Adventure Game</h1>
+            <p>Click below to start your journey.</p>
+            <Link href="/game">
+                <button style={{ padding: '10px 20px', fontSize: '20px', cursor: 'pointer' }}>
+                    Start Game
+                </button>
+            </Link>
+        </div>
+    );
+}
